380f50e00b211e7b207ff3b14707a378d83b0b75
Rhin767 <113712466+Rhin767@users.noreply.github.com>
2026-03-25 01:51:59 -0400

    Delete src directory

--- src/lib/supabase.ts
+++
@@ -1,8 +0,0 @@
-import { createBrowserClient } from '@supabase/ssr'
-
-export function createClient() {
-  return createBrowserClient(
-    'https://vcrssnbmdxoankhkiwhp.supabase.co',
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZjcnNzbmJtZHhvYW5raGtpd2hwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM4MTIzOTAsImV4cCI6MjA4OTM4ODM5MH0.EZ6pPT6j3I_1nlYDTdzPHZaZdhtwET1fbRfT8Nb7Bso'
-  )
-}
